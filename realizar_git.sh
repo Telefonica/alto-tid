a8fecf81541f5c98a972cce13a15142e093ec4d9
root <alejandromunhiz@gmail.com>
2023-08-30 11:50:11 +0000

    1693396211

--- realizar_git.sh
+++ realizar_git.sh
@@ -4,7 +4,7 @@ git init
 git add .
 git reset api/web/certs/*
 git reset alto_fed/*
-git reser kafka_ale/*
+git reset kafka_ale/*
 git add api/web/certs/README.md
 #git reset __pycache__/*
 git reset __pycache__
