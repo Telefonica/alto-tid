41adfdd36e586025e9893b07f1898e51c0b9be86
root <alejandromunhiz@gmail.com>
2023-08-30 12:03:48 +0000

    1693397028

--- realizar_git.sh
+++ realizar_git.sh
@@ -3,10 +3,7 @@
 git init
 git add .
 git reset api/web/certs/*
-git reset alto_fed/*
-git reset kafka_ale/*
-git reset alto_local/*
 #git reset __pycache__/*
 git reset __pycache__
 git commit -m $( date +"%s" )
-git push origin main
+git push origin federation
